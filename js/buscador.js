be2f53964c1352eddccbb8bcafe36a2cd8001d31
ThayliAreliVillegas <20203tn064@utez.edu.mx>
2021-06-25 23:33:57 -0500

    Js examen

---
+++ js/buscador.js
@@ -0,0 +1,5 @@
+
+const formSearch = () => {
+    let name = document.getElementById("buscar").value;
+    alert(`la busqueda con nombre "${name}" no se encuentra, por lo cual intente mas tarde`)
+};
